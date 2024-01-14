bd67a0ae3afd3455108e05c11c79d3abb7649347
tonoy30 <tonoy.sust@gmail.com>
2024-01-15 04:28:04 +0600

    player and gravity added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
-#![warn(clippy::all, clippy::pedantic)]
+#![warn(clippy::pedantic)]
 
 use bracket_lib::prelude::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@'),
+        );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
 
 enum GameMode {
     Menu,
@@ -9,21 +54,26 @@ enum GameMode {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
-            mode: GameMode::Menu
+            player: Player::new(5, 25),
+            frame_time: 0.0,
+            mode: GameMode::Menu,
         }
     }
-    fn play(&mut self) {
-        self.mode = GameMode::End;
-    }
+
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
+
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "Welcome to Flappy Dragon");
@@ -38,11 +88,13 @@ impl State {
             }
         }
     }
+
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
+
         if let Some(key) = ctx.key {
             match key {
                 VirtualKeyCode::P => self.restart(),
@@ -51,15 +103,33 @@ impl State {
             }
         }
     }
+
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
-            GameMode::Playing => self.dead(ctx),
-            GameMode::End => self.play()
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
         }
     }
 }
@@ -68,5 +138,6 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
+
     main_loop(context, State::new())
 }
